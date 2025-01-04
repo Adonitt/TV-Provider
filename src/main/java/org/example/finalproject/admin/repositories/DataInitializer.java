42b29141cb3d174b4933d804ed2420f27c2ec04b
Adonit Halili <adonit.ha12@gmail.com>
2025-01-04 17:55:01 +0100

    u rregullua pricing ne user view me te dhena nga databaza

--- src/main/java/org/example/finalproject/admin/repositories/DataInitializer.java
+++ src/main/java/org/example/finalproject/admin/repositories/DataInitializer.java
@@ -2,21 +2,33 @@ package org.example.finalproject.admin.repositories;
 
 import lombok.RequiredArgsConstructor;
 import org.example.finalproject.admin.models.admin.Admin;
+import org.example.finalproject.admin.models.admin.Packages;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements Runnable {
-    private final AdminRepository repository;
+    private final AdminRepository adminRepository;
+    private final PackageRepository packageRepository;
 
 
     @Override
     public void run() {
-        if (repository.count() == 0) {
+        if (adminRepository.count() == 0) {
             Admin admin = new Admin(1, "Adonit", "Halili", "1252334056", LocalDate.of(2005, 2, 12), "045350345", "Kosovo", "Ali Ajeti 72", "Podujeve", 11000, "adonit.halili@smart-tv.com", "Doni1234.", 19, "Super Admin", "/assets-a/img/656cb1bb-8734-481e-b432-037578ba7ffc_une.jpg", "M");
-            repository.save(admin);
+            adminRepository.save(admin);
+        }
+
+        if (packageRepository.count() == 0) {
+            List<Packages> packages = new ArrayList<>();
+            packages.add(new Packages(1, "Basic", 15.99, 150, 150, "No"));
+            packages.add(new Packages(2, "Premium", 24.99, 190, 200, "Yes"));
+            packages.add(new Packages(3, "Sportive", 19.99, 160, 180, "Yes"));
+            packageRepository.saveAll(packages);
         }
     }
 
--- src/main/java/org/example/finalproject/user/controllers/HomeController2.java
+++ src/main/java/org/example/finalproject/user/controllers/HomeController2.java
@@ -1,10 +1,16 @@
 package org.example.finalproject.user.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.example.finalproject.admin.services.interfaces.AdminService;
+import org.example.finalproject.admin.services.interfaces.PackageService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController2 {
+    private final PackageService packageService;
 
     @GetMapping("/")
     public String index() {
@@ -27,7 +33,8 @@ public class HomeController2 {
     }
 
     @GetMapping("/pricing")
-    public String pricing() {
+    public String pricing(Model model) {
+        model.addAttribute("packages", packageService.findAll());
         return "user-view/pricing";
     }
 
