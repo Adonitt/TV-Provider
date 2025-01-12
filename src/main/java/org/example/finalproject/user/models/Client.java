e1bcf294f813ce0919fff9e41b91ed152abaaeb0
Fatlind Gashi <fatlindgashi88@gmail.com>
2025-01-12 12:59:42 +0100

    added Client Model, Client and Abo Dto, aswell as some html files(invoice, bank information,).

---
+++ src/main/java/org/example/finalproject/user/models/Client.java
@@ -0,0 +1,40 @@
+package org.example.finalproject.user.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+
+}
