9bffedf57b14c54397dc7a8a3c17a949a9c5698d
Fatlind Gashi <fatlindgashi88@gmail.com>
2025-01-30 21:35:39 +0100

    allahu t nimoft bro

--- src/main/java/org/example/finalproject/user/controllers/EpaymentController.java
+++ src/main/java/org/example/finalproject/user/controllers/EpaymentController.java
@@ -1,6 +1,12 @@
 package org.example.finalproject.user.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.example.finalproject.user.dtos.sub.SubDetailsDto;
 import org.example.finalproject.user.dtos.sub.SubDto;
+import org.example.finalproject.user.entities.ClientsEntity;
+import org.example.finalproject.user.mappers.ClientMapper;
+import org.example.finalproject.user.repositories.ClientsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,21 +14,43 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/e-payment")
+@RequiredArgsConstructor
 public class EpaymentController {
+
+    private final ClientsRepository clientsRepository;
+    private final ClientMapper clientMapper;
 
     @GetMapping("")
     public String ePayment(Model model) {
-        model.addAttribute("subDto", new SubDto());
+        model.addAttribute("subDto", new SubDto()); // Empty DTO for input
         return "user-view/e-payment/e-payment";
     }
 
     @PostMapping("")
-    public String ePayment(@ModelAttribute SubDto subDto, Model model) {
-        model.addAttribute("message", "Client founded successfully. Please fill in the bank credentials.");
+    public String validateClient(@ModelAttribute SubDto subDto, Model model) {
+        Optional<ClientsEntity> clientOpt = clientsRepository.findByClientNrAndLastName(subDto.getClientNr(), subDto.getLastName());
+
+        if (clientOpt.isPresent()) {
+            // You could choose to populate the SubDto with additional data here
+            ClientsEntity client = clientOpt.get();
+            subDto.setLastName(client.getLastName());
+
+            // Add subDto to the model
+            model.addAttribute("subDto", subDto);
+
+            // Redirect to details page
+            return "user-view/e-payment/details";
+        }
+
+        model.addAttribute("errorMessage", "Client does not exist. Please check your details.");
         return "user-view/e-payment/e-payment";
     }
+
+
 
 
     @GetMapping("/bank-information")
